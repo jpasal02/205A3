68c5f63c2ad30c7bcb094b72b3d707bbe814ed58
jpasal02 <jasmina-pasalic@hotmail.com>
2015-09-23 16:55:09 +1000

    DAO

--- ITC515_NewLibrary/src/library/daos/BookDAO.java
+++ ITC515_NewLibrary/src/library/daos/BookDAO.java
@@ -1,9 +1,52 @@
 package library.daos;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 import library.interfaces.daos.IBookDAO;
+import library.interfaces.daos.IBookHelper;
+import library.interfaces.entities.IBook;
 
 public class BookDAO implements IBookDAO{
 	
-	public BookDAO (String Autho)
+	private IBookHelper helper; 
+	private List<IBook> map;
+	private int nextID;
+	
+	
+	public BookDAO (IBookHelper helper){
+		if (helper == null)
+			throw new IllegalArgumentException ("Error: Helper");
+	
+		
+	}
+	
+	public IBook addBook(String author, String Title, String callNumber){
+		IBook book = helper.makeBook(author, Title, callNumber, nextID++);
+		map.add(book);
+		return book;
+		
+		
+	}
+	
+	public IBook getBookByID(int id){
+		
+	}
+	
+	public List<IBook> listBooks(){
+		
+	}
+	
+	public List<IBook> findBooksByAuthor(String author){
+		
+	}
+	
+	public List<IBook> findBooksByTitle(String title){
+		
+	}
+	public List<IBook> findBooksByAuthorTitle(String author, String title){
+		
+	}
 
 }
