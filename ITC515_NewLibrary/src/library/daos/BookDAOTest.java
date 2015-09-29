5dfce0ee18dfda7c58d59c59998156f6cb9bf32e
jpasal02 <jasmina-pasalic@hotmail.com>
2015-09-29 20:15:04 +1000

    Dao Class made

---
+++ ITC515_NewLibrary/src/library/daos/BookDAOTest.java
@@ -0,0 +1,14 @@
+package library.daos;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BookDAOTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
