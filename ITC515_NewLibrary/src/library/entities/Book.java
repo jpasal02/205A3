5d508ccfa9dde550197e6a138d1843e218260bcf
jpasal02 <jasmina-pasalic@hotmail.com>
2015-10-02 22:02:59 +1000

    Attempting to make program work

--- ITC515_NewLibrary/src/library/entities/Book.java
+++ ITC515_NewLibrary/src/library/entities/Book.java
@@ -108,6 +108,8 @@ public class Book implements IBook{
 		this.state = state;
 		
 	}
+	
+	
 
 	
 		
--- ITC515_NewLibrary/src/tests/library/IntergrationTest.java
+++ ITC515_NewLibrary/src/tests/library/IntergrationTest.java
@@ -1,57 +1,66 @@
 package tests.library;
 
+import static org.junit.Assert.*;
+
+import java.sql.Date;
+import java.util.Calendar;
+
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
-import library.BorrowUC_UI;
-import library.interfaces.daos.IBookDAO;
-import library.interfaces.daos.ILoanDAO;
-import library.interfaces.daos.IMemberDAO;
-import library.interfaces.hardware.ICardReader;
-import library.interfaces.hardware.IDisplay;
-import library.interfaces.hardware.IPrinter;
-import library.interfaces.hardware.IScanner;
+import library.entities.Book;
+import library.entities.Loan;
+import library.interfaces.entities.IBook;
+import library.interfaces.entities.ILoan;
+import library.interfaces.entities.IMember;
 
 public class IntergrationTest {
-	
-	private IBookDAO bookDAO;
-	private ILoanDAO loanDAO; 
-	private IMemberDAO memeberDAO; 
-	
-	private ICardReader cardReader;
-	private IPrinter printer; 
-	private IScanner scanner; 
-	private IDisplay display; 
-	private BorrowUC_UI ui; 
-	
-	public void set() {
-		
-	}
-	
-	public void Initialize(){
-		
-		
-	}
-	
-	@Test 
-	public void cardSwipe1(){
-		
-	}
-	@Test 
-	public void cardSwipe2(){
-		
-	}
-	@Test 
-	public void cardSwipe3(){
-	
-	}
-	@Test 
-	public void cardSwipe4(){
-	
-	}
-	public void cardSwipe5(){
-	
-	}
-	
-	
 
+	IBook book;
+	IMember member;
+	ILoan loan;
+	
+	private String author;
+	private String title;
+	private String callNumber;
+	private int id;
+
+	Date borrowDate, dueDate;
+	Calendar calender;
+	
+	@Before
+	public void setUp() throws Exception {
+		
+		author = "Bob";
+		title = "Jones";
+		callNumber = "123456";
+		id = 1;
+		
+		book = new Book(author, title, callNumber, id);
+		
+	}
+	
+	@After
+	public void clearSetUp() throws Exception
+	{
+		book = null;
+	}
+	
+	@Test
+	public void testBook()
+	{
+		IBook book = new Book(author, title, callNumber, id);
+		assertTrue(book instanceof Book);
+	}
+	
+	@Test
+	public void testBorrow()
+	{		
+		Loan loan = new Loan(book, member, borrowDate, dueDate);
+				
+		book.borrow(loan); 
+		
+		assertEquals(loan.getBook(), book);
+	}
 }
